b305b36b5f13a16c82d6adfcbfcdf5d6a953bab2
William L. Anderson <band@users.noreply.github.com>
2024-11-13 12:39:49 -0600

    vercel build updates

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Rest of the script...
-
 # Install Python
 curl -L https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh -o miniforge.sh
 bash miniforge.sh -b -p $HOME/miniforge
@@ -10,4 +8,5 @@ export PATH=$HOME/miniforge/bin:$PATH
 # Install dependencies and run build
 cd .nxc
 pip install -r requirements.txt
-LOGLEVEL=INFO nxc build -i ... -o ./output --lunr --commits
+npm ci
+LOGLEVEL=INFO nxc build -i .. -o ./output --lunr --commits
