109ee1f641cc28d5a7a33f3e13a0b134ad9a1e5b
William L. Anderson <band@users.noreply.github.com>
2024-11-13 06:48:18 -0600

    vercel.community help

---
+++ nxcbuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Rest of the script...
+
+# Install Python
+curl -L https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh -o miniforge.sh
+bash miniforge.sh -b -p $HOME/miniforge
+export PATH=$HOME/miniforge/bin:$PATH
+
+# Install dependencies and run build
+cd .nxc
+pip install -r requirements.txt
+nxc build -i ... -o ./output --lunr --commits
